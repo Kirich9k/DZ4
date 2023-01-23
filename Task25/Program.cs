7c5c8021eeef33029996760ffb22a4d92ab9d3e6
Kirich9k <kirya65@gmail.com>
2023-01-23 22:03:17 +0300

    Task 25 accomplished

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень, в которую надо возвести это число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for (int i = 1; i <= b; i++)
+{
+    result = result * a;
+}
+Console.WriteLine(result);
