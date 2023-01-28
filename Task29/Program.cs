a0f8c080ff2557e85790dd372d7e7cf2c8c18143
Kirich9k <kirya65@gmail.com>
2023-01-28 20:31:33 +0300

    Task 29 accomplished

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+Console.WriteLine($"Введите {i+1} элемент массива: ");
+array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"Массив: [{string.Join(", ", array)}]");
