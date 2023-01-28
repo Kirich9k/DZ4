5d2679904a9861874fb1a0269dd10c313f2313a8
Kirich9k <kirya65@gmail.com>
2023-01-28 20:30:11 +0300

    Task 27 accomplished

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string? digitForElementSum = Console.ReadLine();
+int sumOfElements = 0;
+for (int n = 0; n < digitForElementSum.Length; n++)
+{
+sumOfElements = sumOfElements + int.Parse(digitForElementSum[n].ToString());
+}
+Console.WriteLine(sumOfElements);
